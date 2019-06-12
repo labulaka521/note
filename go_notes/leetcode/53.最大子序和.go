db5cd37e806272f9c4066e76a2d99e184c8a7f8e
labulaka <labulakalia@gmail.com>
2019-06-12 09:59:44 +0800

    Update 53.最大子序和.go

--- go_notes/leetcode/53.最大子序和.go
+++ go_notes/leetcode/53.最大子序和.go
@@ -7,7 +7,7 @@ func maxSubArray(nums []int) int {
 	var maxsum, cursum int
 	maxsum = nums[0]
 	for _, value := range nums {
-		cursum += value
+		cursum += value          // cursum会一直增加
 		if cursum > maxsum {	// 比较加一个数后和当前的最大值哪个大
 			maxsum = cursum  // 保存当前的最大值
 		}
