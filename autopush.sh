481eecca603c418739747cf88dc2cb5196d21869
labulaka522 <labulakalia@gmail.com>
2019-06-15 08:48:35 +0800

    update notes

--- autopush.sh
+++ autopush.sh
@@ -1,6 +1,6 @@
 export LANG=en_US.UTF-8
 pidfile=/tmp/autopush.pid
-cd /Users/wanglitao/Dropbox/notes
+cd /Users/wanglitao/Dropbox/note
 case $1 in 
     start)
     if [[ -f $pidfile ]];then
---
+++ go_notes/leetcode/58.最后一个单词的长度.go
@@ -0,0 +1,9 @@
+/*
+ * @lc app=leetcode.cn id=58 lang=golang
+ *
+ * [58] 最后一个单词的长度
+ */
+func lengthOfLastWord(s string) int {
+    
+}
+
